11ce2060e700c7a0147393b18783f46d43b86c35
rogepi <zzr1119@outlook.com>
2021-02-02 19:07:28 +0800

    edit main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ import router from "./router";
 import Element from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
-
 Vue.use(router);
 Vue.use(Element);
-
 
 Vue.config.productionTip = false;
 
